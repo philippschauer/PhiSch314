70303ae24eca4725c9bc8b3ee6c05ed82b259562
plschauer <62629123+plschauer@users.noreply.github.com>
2024-03-12 20:54:33 +0100

    First Try End-to-End Pipeline

---
+++ api_call.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+import numpy as np
+from gensim.models import Word2Vec
+import pickle
+import keras
+
+# Load all the models and tokenizers
+with open('tokenizer.pkl', 'rb') as f:
+    tokenizer = pickle.load(f)
+
+with open('label_encoder.pkl', 'rb') as f:
+    label_encoder = pickle.load(f)
+
+model = keras.models.load_model('model.h5')
+
+word2vec_model = Word2Vec.load("word2vec_model.bin")
+
+# Initialize FastAPI app
+app = FastAPI()
+
+# Define the function that embeds the input
+def text_to_embeddings(text, word2vec_model):
+    tokens = tokenizer(text)
+    embeddings = []
+    for token in tokens:
+        if token in word2vec_model.wv:
+            embeddings.append(word2vec_model.wv[token])
+    if embeddings:
+        return np.mean(embeddings, axis=0) 
+    else:
+        return np.zeros(50)
+
+
+# Predict the label of the word  
+@app.get("/predict")
+def predict_word(word):
+    new_word_embedding = text_to_embeddings(word, word2vec_model).reshape(1, -1) 
+    # Predict the label for the embedded word
+    predicted_probabilities = model.predict(new_word_embedding)
+    predicted_label_index = predicted_probabilities.argmax(axis=1)[0]
+    predicted_label = label_encoder.inverse_transform([predicted_label_index])
+    return {"predicted_label": predicted_label[0]}
+
+
+# Run the FastAPI app
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
